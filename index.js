84e11722e92172ca3ab05db6ae28eaae07eb7895
Unityied <jonasehero@gmail.com>
2024-05-27 15:22:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 NRW | RolePlay`);
+  console.log(`🔗 Welcome to NRW`);
 });
 
 
