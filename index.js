38cdf8c7b2abecf4c02342602ce1e0b1e8cbef59
nuxewux <164436598+nuxewux@users.noreply.github.com>
2024-09-08 23:15:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Best Bot Ever"];
 
 
 let currentIndex = 0;
